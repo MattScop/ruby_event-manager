783e78549184b22e66d7fc5575d6712f9090c90a
MattScop <matteo.scopece@outlook.com>
2022-10-05 08:55:22 +0200

    Use CSV library

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -9,9 +9,27 @@ puts 'Event Manager Initialized!'
     #     puts line
     # end
 # GET INFO ONLY FROM A SPECIFIC COLUMN
-    content = File.readlines('event_attendees.csv')
-    content.each_with_index do |line, idx|
-        next if idx == 0 # skip the header
-        names = line.split(',')[2]
-        puts names
+    # content = File.readlines('event_attendees.csv')
+    # content.each_with_index do |line, idx|
+    #     next if idx == 0 # skip the header
+    #     names = line.split(',')[2]
+    #     puts names
+    # end
+# SWITCHING TO CSV LIBRARY
+    require 'csv'
+
+    def clean_zip_codes(zip_code)
+        # if the zip code is missing, then add 00000
+        # if the zip code is more than five digits, truncate it to the first five digits
+        # if the zip code is less than five digits, add zeros to the front until it becomes five digits
+        # if the zip code is exactly five digits, assume that it is ok
+        zip_code.to_s.rjust(5, '0')[0..4]
     end
+    
+    contents = CSV.open('../event_attendees.csv', headers: true, header_converters: :symbol)
+    contents.each do |line|
+        name = line[:first_name]
+        zip_code = clean_zip_codes(line[:zipcode])
+        puts "#{name} #{zip_code}"
+    end
+    
